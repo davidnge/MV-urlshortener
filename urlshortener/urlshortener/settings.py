82a26e30f2ed9fe17fa0a65b9bc89b7ec01558c2
nge chee keen <david.nnck@gmail.com>
2015-03-06 11:56:34 +0800

    commit static files

--- urlshortener/urlshortener/settings.py
+++ urlshortener/urlshortener/settings.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -62,13 +65,24 @@ BITLY_API_KEY = 'R_f5231f7a4063408e81202b9725a6c6fa'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd6sibqnhk0ajhp',
+        'USER': 'eaocqbtovogznf',
+        'PASSWORD': '193zs5otP-aBlGSHGPY9OAPMLU',
+        'HOST': 'ec2-50-19-249-214.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -101,14 +115,7 @@ USE_TZ = True
 
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "root")
 STATIC_URL = '/static/'
