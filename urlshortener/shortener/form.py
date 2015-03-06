b5e519a7ab71b812fd16cf57b578e712160096d9
nge chee keen <david.nnck@gmail.com>
2015-03-06 15:02:31 +0800

    removed redundant shortener folder

--- urlshortener/shortener/form.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class SubmitForm(forms.Form):
-	urlLink = forms.URLField(label="", max_length=200, widget=forms.TextInput(attrs={'placeholder': 'http://www.example.com'}))
